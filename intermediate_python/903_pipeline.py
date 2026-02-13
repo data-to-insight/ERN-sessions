19b6c7adfa1d2ce6107c462b9cf47bc3d78fe3a7
William Levack Payne <108789355+WillLP-code@users.noreply.github.com>
2026-02-13 15:31:53 +0000

    session 3 finished

--- intermediate_python/903_pipeline.py
+++ intermediate_python/903_pipeline.py
@@ -12,8 +12,14 @@ from sqlalchemy import (
 from utils import clean_903_table
 from datetime import datetime
 
+# Import in session 3
+from utils import group_calculation, time_difference
+from dateutil.relativedelta import relativedelta
+import numpy as np
+
 # Set variables in session 2
-collection_end = datetime(2025, 3, 31)
+collection_year = 2014
+collection_end = datetime(collection_year, 3, 31)
 
 engine_903 = create_engine(
     "sqlite+pysqlite:////workspaces/ERN-sessions/intermediate_python/903_database.db"
@@ -53,6 +59,66 @@ national_characteristics = pd.read_excel(
 for key, df in dfs.items():
     dfs[key] = clean_903_table(df, collection_end=collection_end)
 
-print(dfs["header"])
-
 ########### END OF SESSION 2 ###########
+# If we are prepping data for Power BI/Excel we want to organise our data differently to prep for Python.
+# For external dashboarding products we want everything calculated already so we can easily slice it
+# and keep the dashboard nice and lightweight, not performing any calculations
+
+# We need an ampty dict to store our measures in:
+measures = {}
+
+# Calculate groupbys
+# We'll need a reusable function here that can groupby a value in a colum (e.g. ethnicity) that outputs
+# in the way we want so we can reuse it every time we want to group by something
+
+# Total CYP in header by ethnicity
+# Let's do it for ethnicity then turn it into a function - let's get a count and a percent
+
+# grouped = dfs['header'].groupby(['ETHNICITY']).size()
+# grouped = grouped.to_frame('Header - Ethnicities - Count').reset_index()
+# grouped = grouped.rename(columns={'ETHNICITY':'Ethnicities'})
+
+# grouped['Header - Ethnicities - Percentage'] =  (grouped['Header - Ethnicities - Count'] / grouped['Header - Ethnicities - Count'].sum() ) * 100
+
+# print(grouped)
+# print(grouped['Header - Ethnicities - Percentage'].sum())
+# Let's make the function in utils.py
+measures["Header by ethnicity"] = group_calculation(
+    dfs["header"], "ETHNICITY", "Header - Ethnicities"
+)
+
+# Let's show now how easy it is to do the same using our age buckets
+measures["Header by age"] = group_calculation(
+    dfs["header"], "AGE_BUCKETS", "Header - Age"
+)
+
+
+# Now let's see why we did it like htis for our final outputs.
+# We won't keep this in but we will use it later
+output_table = pd.concat([measures["Header by ethnicity"], measures["Header by age"]])
+
+# Calculate time periods
+# Whether we want exact numbers of buckets, we need a way to calculate time periods (e.g. time children have)
+# been in a placement. It's good to have contingency for business days in there too as many CS
+# measures take note of business days
+
+# Same premise, make it as a normal calculation then turn it into a function
+# dfs["missing"]["MISSING_DURATION"] = dfs["missing"].apply(
+#     lambda x: relativedelta(x["MIS_START_dt"], x["MIS_END_dt"]).normalized().days, axis=1
+# )
+
+# dfs["missing"]['MISSING_DURATION'] = dfs["missing"]["MIS_END_dt"] - dfs["missing"]["MIS_START_dt"]
+
+# or for working days:
+# We need to convert these to np datetime 64s rather than pd datetimes for this calculation
+# dfs["missing"]['MISSING_DURATION'] = np.busday_count(dfs["missing"]["MIS_START_dt"].values.astype('datetime64[D]'), dfs["missing"]["MIS_END_dt"].values.astype('datetime64[D]'))
+
+dfs["missing"]["MISSING_DURATION"] = time_difference(
+    dfs["missing"]["MIS_START_dt"], dfs["missing"]["MIS_END_dt"], True
+)
+
+# We might also think about making some type of function that groups this by numbers of days.
+# That will depend on what we are looking at, however, for instance we might want under and over
+# 45 days for assessments, but that would make no sense for placement lengths.
+# We could .apply() a function like the one for age buckets in each different instance for this
+print(dfs["missing"])
--- intermediate_python/utils.py
+++ intermediate_python/utils.py
@@ -6,6 +6,9 @@ from config_903 import EthnicSubcategories, DateCols903
 from dateutil.relativedelta import relativedelta
 from datetime import datetime
 
+# Import in session 2
+import numpy as np
+
 
 def format_dates(column):
     # Will make dates for Y/m/d or d/m/Y
@@ -22,16 +25,18 @@ def format_dates(column):
 
 def calculate_age_buckets(age):
     # Used to make age buckets matching published data
-    if age < 5:
-        return "1 to 4 years"
+    if age < 1:
+        return "a ) Under 1 year"
+    elif age < 5:
+        return "b) 1 to 4 years"
     elif age < 10:
-        return "5 to 9 years"
+        return "c) 5 to 9 years"
     elif age < 16:
-        return "10 to 16 years"
+        return "d) 10 to 16 years"
     elif age >= 16:
-        return "16 years and over"
+        return "e) 16 years and over"
     else:
-        return "Age error"
+        return "f) Age error"
 
 
 def clean_903_table(df: pd.DataFrame, collection_end: pd.Timestamp):
@@ -50,9 +55,38 @@ def clean_903_table(df: pd.DataFrame, collection_end: pd.Timestamp):
         )
 
     if "DOB_dt" in clean_df.columns:
+        # print(clean_df['DOB_dt'].max()) - we don't need this, we can just use it to find the latest DOB
         clean_df["AGE"] = clean_df["DOB_dt"].apply(
-            lambda x: relativedelta(dt1=collection_end, dt2=x).years
+            lambda x: relativedelta(dt1=collection_end, dt2=x).normalized().years
         )
         clean_df["AGE_BUCKETS"] = clean_df["AGE"].apply(calculate_age_buckets)
 
     return clean_df
+
+
+########### END OF SESSION 2 ###################
+def group_calculation(df, col_to_group, measure_name):
+    grouped = df.groupby([col_to_group]).size()
+    grouped = grouped.to_frame("Count").reset_index()
+    grouped = grouped.rename(columns={col_to_group: "Value"})
+
+    grouped["Percentage"] = (grouped["Count"] / grouped["Count"].sum()) * 100
+
+    grouped["Measure"] = measure_name
+
+    grouped_ordered = grouped[["Measure", "Value", "Count", "Percentage"]]
+
+    return grouped_ordered
+
+
+def time_difference(start_col, end_col, business_days=False):
+    if business_days:
+        time_diff = np.busday_count(
+            start_col.values.astype("datetime64[D]"),
+            end_col.values.astype("datetime64[D]"),
+        )
+    else:
+        time_diff = end_col - start_col
+        time_diff = time_diff / pd.Timedelta(days=1)
+
+    return time_diff.astype("int")
